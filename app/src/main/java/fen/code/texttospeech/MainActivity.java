bc321cca25853e09a95aae570558115529aef992
Fenton Martin <fentonxm@gmail.com>
2017-09-08 14:11:53 +0700

    Init Button Speech to Text and Speech Recognition

--- app/src/main/java/fen/code/texttospeech/MainActivity.java
+++ app/src/main/java/fen/code/texttospeech/MainActivity.java
@@ -1,7 +1,10 @@
 package fen.code.texttospeech;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,10 +13,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int REQ_CODE_SPEECH_INPUT = 100;
     Button textToSpeech, speechToText;
     EditText textInput;
     TextView textResult;
@@ -68,6 +73,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /* Init Button Speech to Text */
+        speechToText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* Start Speech Recognition */
+                speechRecognition();
+            }
+        });
+
     }
 
     public void onPause() {
@@ -77,5 +91,47 @@ public class MainActivity extends AppCompatActivity {
             speech.shutdown();
         }
         super.onPause();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        /* Add Recognizer Result */
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    /* Get Recognizer Result */
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    /* Set Recognizer Result */
+                    textResult.setText(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+    /* Add Speech Recognition */
+    public void speechRecognition() {
+        /* Add Recognizer Intent */
+        Intent intent = new Intent(RecognizerIntent
+                .ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent
+                .EXTRA_LANGUAGE_MODEL, RecognizerIntent
+                .LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent
+                .EXTRA_LANGUAGE, Locale.ENGLISH);
+        intent.putExtra(RecognizerIntent
+                .EXTRA_PROMPT, getString(R.string.speech_prompt));
+
+        try {
+            /* Start Activity For Result */
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            /* Set Phone Not Supported Recognizer */
+            Toast.makeText(getApplicationContext(), getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
