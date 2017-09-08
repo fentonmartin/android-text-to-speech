e53da41b9a07f3efed2789a87d278a40e9add707
Fenton Martin <fentonxm@gmail.com>
2017-09-08 13:43:05 +0700

    Add views declaration

--- app/src/main/java/fen/code/texttospeech/MainActivity.java
+++ app/src/main/java/fen/code/texttospeech/MainActivity.java
@@ -1,13 +1,27 @@
 package fen.code.texttospeech;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button textToSpeech, speechToText;
+    EditText textInput;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Add views declaration */
+        textToSpeech = (Button) findViewById(R.id.text_to_speech_btn);
+        speechToText = (Button) findViewById(R.id.speech_to_text_btn);
+        textInput = (EditText) findViewById(R.id.text_input);
+        textResult = (TextView) findViewById(R.id.text_result);
+        
     }
 }
